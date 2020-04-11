3ca4b3342d4c782c966acef20b883c5211b1cfa3
NonKhuna <mathnon56@gmail.com>
2020-04-11 23:16:50 +0700

    add imagesongkran

---
+++ Assets/scripts/gallarySystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gallarySystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public MeshRenderer face;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void picImage()
+    {
+        NativeGallery.Permission permission = NativeGallery.GetImageFromGallery((path) =>
+        {
+            if(path != null){
+                Texture2D texture = NativeGallery.LoadImageAtPath(path);
+                if(texture == null) {
+                    Debug.Log("Could't load texture from" + path);
+                    return;
+                }
+                texture.Resize(texture.width,texture.width);
+                face.material.mainTexture = texture;
+
+            }
+        },"select a image","image/*");
+    }
+}
