a36cfb3b91441c590d61bebc2324391d8a084930
NonKhuna <mathnon56@gmail.com>
2020-04-13 13:13:21 +0700

    add guide

---
+++ Assets/scripts/UIsystem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIsystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject guide1;
+    public GameObject guide2;
+    public GameObject guidebutton;
+    void Start()
+    {
+        guide1.SetActive(true);
+        guidebutton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Close(){
+        guide1.SetActive(false);
+        guide2.SetActive(false);
+        guidebutton.SetActive(true);
+    }
+
+    public void next(){
+        guide1.SetActive(false);
+        guide2.SetActive(true);
+        guidebutton.SetActive(false);
+    }
+
+    public void guideOpen(){
+        guide1.SetActive(true);
+        guide2.SetActive(false);
+        guidebutton.SetActive(false);
+    }
+}
